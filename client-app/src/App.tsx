9c912c7da1eb8e4ef26d73d6ee11269300f6965d
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-03-01 23:27:01 +0200

    feat: fetch expenses from api

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,12 +1,26 @@
 import { Box, Typography } from '@mui/material';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Expense from './types/product';
 
-const App: React.FC = () => (
-  <Box>
-    <Typography variant="h3" component="h1">
-      Expense Tracker
-    </Typography>
-  </Box>
-);
+const App: React.FC = () => {
+  const [expenses, setExpenses] = useState<Expense[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/expenses')
+      .then((res) => res.json())
+      .then((data) => setExpenses(data));
+  }, []);
+
+  return (
+    <Box>
+      <Typography variant="h3" component="h1">
+        Expense Tracker
+      </Typography>
+      {expenses.map((expense) => (
+        <Typography key={expense.id}>{expense.title}</Typography>
+      ))}
+    </Box>
+  );
+};
 
 export default App;
