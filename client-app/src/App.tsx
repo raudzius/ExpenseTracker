1b4b752bf19bf3cc1d4a7ff0d75c485a8f64f531
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-03-02 01:12:49 +0200

    feat: expense table foundation

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,7 +1,8 @@
 import {
-  Box, Typography,
+  Box, Paper, Table, TableBody, TableContainer, Typography,
 } from '@mui/material';
 import React, { useEffect, useState } from 'react';
+import ExpenseItem from './components/ExpenseItem';
 import { Expense, FetchedExpense } from './types/product';
 
 const App: React.FC = () => {
@@ -24,9 +25,15 @@ const App: React.FC = () => {
       <Typography variant="h3" component="h1">
         Expense Tracker
       </Typography>
-      {expenses.map((expense) => (
-        <Typography key={expense.id}>{expense.title}</Typography>
-      ))}
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }}>
+          <TableBody>
+            {expenses.map((expense) => (
+              <ExpenseItem key={expense.id} expense={expense} />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Box>
   );
 };
